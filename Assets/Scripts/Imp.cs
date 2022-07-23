8c6511b51e35dba26bea930932c3100f4d3f81f4
tinybitofheaven <abc9295@nyu.edu>
2022-07-22 23:59:17 -0700

    Add Imp script, more sprites

---
+++ Assets/Scripts/Imp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Imp : MonoBehaviour
+{
+    enum State { IDLE, ATTACK, DEATH, TELEPORT }
+    private State currState = State.IDLE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
