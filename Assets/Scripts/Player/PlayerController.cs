153e972804c5df3e76c071806d4e40b6ae8dcaf7
Trigon1015 <zz2464@nyu.edu>
2022-07-26 00:09:04 +0800

    Finish player movement, jumping, and shoot1

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     private bool Grounded;
     public LayerMask whatIsGround;
 
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +25,14 @@ public class PlayerController : MonoBehaviour
     {
         //movement
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+        if(rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(rb.velocity.x >0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
 
         //jump
         Grounded = Physics2D.OverlapCircle(groundPoint.position, 0.2f, whatIsGround);
@@ -31,5 +40,21 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        //shoot1
+        if(Input.GetKey(KeyCode.Q) )
+        {
+            anim.SetTrigger("Shoot1");
+            if(anim.GetCurrentAnimatorStateInfo(0).IsName("Player_shoot1"))
+            {
+                rb.velocity = new Vector2(0f, rb.velocity.y);
+            }
+            
+        }
+
+
+        //animation 
+        anim.SetBool("Grounded", Grounded);
+        anim.SetFloat("speed", Mathf.Abs(rb.velocity.x));
     }
 }
