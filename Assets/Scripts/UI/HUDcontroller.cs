c23946ce4010d7438255180c8e513a5b9365ed6f
asaagohan <arnee.wenn@gmail.com>
2022-08-09 03:24:42 -0700

    a
    a

--- Assets/Scripts/UI/HUDcontroller.cs
+++ Assets/Scripts/UI/HUDcontroller.cs
@@ -6,13 +6,15 @@ using TMPro;
 
 public class HUDcontroller : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI cointxt, difftxt, missiontxt, lvltxt, hptxt;
-    [SerializeField] private GameObject immunebar;
+    [SerializeField] private TextMeshProUGUI cointxt, difftxt, missiontxt, lvltxt, hptxt, CDtxt2, CDtxt3, CDtxt4;
+    [SerializeField] private GameObject immunebar, abilityCD2, abilityCD3, abilityCD4;
     [SerializeField] private RawImage damagebar, hpbar, expbar;
 
+    private PlayerController player;
     private void Start()
     {
         immunebar.SetActive(false);
+        player = FindObjectOfType<PlayerController>();
     }
     private void Update() 
     {
@@ -36,6 +38,34 @@ public class HUDcontroller : MonoBehaviour
         {
             immunebar.SetActive(false);
         }
-        //if ()
+
+
+        if (player.Shoot2Counter!=0 && player.Shoot2Counter<player.shoot2CD)
+        {
+            abilityCD2.SetActive(true);
+            CDtxt2.text = (int)(player.Shoot2Counter)+"";
+        }
+        else
+        {
+            abilityCD2.SetActive(false);
+        }
+        if (player.Shoot3Counter!=0 && player.Shoot3Counter<player.shoot3CD)
+        {
+            abilityCD3.SetActive(true);
+            CDtxt3.text = (int)(player.Shoot3Counter)+"";
+        }
+        else
+        {
+            abilityCD3.SetActive(false);
+        }
+        if (player.Shoot4Counter!=0 && player.Shoot4Counter<player.shoot4CD)
+        {
+            abilityCD4.SetActive(true);
+            CDtxt4.text = (int)(player.Shoot4Counter)+"";
+        }
+        else
+        {
+            abilityCD4.SetActive(false);
+        }
     }
 }
