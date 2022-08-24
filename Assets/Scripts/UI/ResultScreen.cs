16365f87e7dd0b479cc95510725c4396471a1bca
asaagohan <arnee.wenn@gmail.com>
2022-08-23 21:18:24 -0700

    a
    a

--- Assets/Scripts/UI/ResultScreen.cs
+++ Assets/Scripts/UI/ResultScreen.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ResultScreen : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI levelTxt, timeTxt, killTxt, BossTxt, itemTxt, goldTxt, purchaseTxt, TitleTxt;
-    public int killCount = 0;
-
+    [SerializeField] private TextMeshProUGUI levelTxt, timeTxt, killTxt, killedbyTxt, BossTxt, itemTxt, goldTxt, purchaseTxt, TitleTxt;
+    
     void Update()
     {
         Time.timeScale = 0;
         levelTxt.text = 1 + GameManager.FindInstance().exp / 100+"";
         timeTxt.text = FindObjectOfType<Timer>().timeText.text;
         goldTxt.text = GameManager.FindInstance().coins+"";
-        killTxt.text = killCount+"";
+        killTxt.text = GameManager.FindInstance().kills+"";
+        BossTxt.text = GameManager.FindInstance().bossKill+"";
+        itemTxt.text = GameManager.FindInstance().items+"";
+        purchaseTxt.text = GameManager.FindInstance().purchases+"";
+        killedbyTxt.text = "Killed by: "+GameManager.FindInstance().killedBy;
     }
 
     public void showWinScreen() {
@@ -23,5 +27,15 @@ public class ResultScreen : MonoBehaviour
     }
 
     public void showLossScreen() {
+    }
+
+    public void goback()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void tryagain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
